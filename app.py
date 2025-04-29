85bd7fde83990141379429b3850ab5ef73025060
Matt Shields <mashields@ptc.com>
2025-04-29 10:20:15 -0400

    multiple mates

--- app.py
+++ app.py
@@ -3,14 +3,16 @@ import json
 import requests
 from datetime import datetime, timedelta
 import os
+from numpy.random import randint
+from math import degrees, pi
 
 # Create the application.
 app = Flask(__name__)
 
 # Some global variables
-OS_DOMAIN, OS_USER_ID, USER_NAME = None, None, None
+OS_DOMAIN, OS_USER_ID, USER_NAME, HOST = None, None, None, None
 ACCESS_TOKEN, REFRESH_TOKEN, EXPIRES_AT = None, None, None
-DID, WID, EID, CONFIG, ETYPE = None, None, None, None, None
+DID, WID, EID, CONFIG, FUNCTION = None, None, None, None, None
 
 # Get your API keys from a json file
 with open('oauth_key.json') as f: 
@@ -18,10 +20,20 @@ with open('oauth_key.json') as f:
     OAUTH_CLIENT_ID = oauth_keys['OAUTH_CLIENT_ID']
     OAUTH_CLIENT_SECRET = oauth_keys['OAUTH_CLIENT_SECRET']
 
+def handleReturn(FUNCTION):
+    if FUNCTION == 'elements':
+        return redirect(url_for('elements'))
+    elif FUNCTION == 'colors':
+        return redirect(url_for('colors'))
+    elif FUNCTION == 'robot':
+        return redirect(url_for('robot'))
+    else:    
+        return redirect(url_for('index'))
+
 # === The main page ===
 @app.route('/')
 def index():
-
+    # Just to test the API, lets get all the parts in this element
     response = requests.get(
         os.path.join(
             OS_DOMAIN, 
@@ -43,6 +55,82 @@ def index():
             arr = [p["name"], p["partId"]]
             PARTS.append(arr)
 
+    return render_template('index.html', parts=PARTS)
+
+@app.route('/robot')
+def robot():
+    response = requests.get(
+        os.path.join(
+            OS_DOMAIN,
+            "api/assemblies/d/{}/w/{}/e/{}/matevalues".format(
+            DID, WID, EID
+            )
+        ),
+        headers={
+            "Accept": "application/json;charset=UTF-8; qs=0.09",
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + ACCESS_TOKEN
+        }
+    )
+
+    MATES = []
+    MATES_DEG = []
+
+    response = response.json()
+
+    for mate in response["mateValues"]:
+        if mate['mateName'].split('_')[0] == "Arm":
+            MATES_DEG.append((mate['mateName'],degrees(mate['rotationZ'])))
+            MATES.append(mate)
+
+    msg = ""
+    if request.args.get('reset'):
+        msg = "Reset"
+        for mate in MATES:
+            mate['rotationZ'] = 0
+
+        response = requests.post(
+            os.path.join(
+                OS_DOMAIN,
+                "api/assemblies/d/{}/w/{}/e/{}/matevalues".format(
+                DID, WID, EID
+                )
+            ),
+            headers={
+                "Accept": "application/json;charset=UTF-8; qs=0.09",
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + ACCESS_TOKEN
+            },
+            json = {"mateValues":MATES}
+        )
+        response = response.json()  
+
+    if request.args.get('mate_values'):
+        msg = request.args.get('mate_values')
+        for mate in MATES:
+            mate['rotationZ'] = mate['rotationZ'] + pi/8
+
+        response = requests.post(
+            os.path.join(
+                OS_DOMAIN,
+                "api/assemblies/d/{}/w/{}/e/{}/matevalues".format(
+                DID, WID, EID
+                )
+            ),
+            headers={
+                "Accept": "application/json;charset=UTF-8; qs=0.09",
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + ACCESS_TOKEN
+            },
+            json = {"mateValues":MATES}
+        )
+        response = response.json()        
+    
+    return render_template('robot.html', mate_values = MATES_DEG, msg=msg)
+
+@app.route('/elements')
+def elements():
+    # Just to test the API, lets get all of the elements in the workspace
     response = requests.get(
         os.path.join(
             OS_DOMAIN,
@@ -58,21 +146,28 @@ def index():
     )
     
     ELEMENTS = []
-    VSID = ""
     if response.ok: 
         response = response.json() 
         for e in response:
             arr = [e["name"], e["elementType"]]
             ELEMENTS.append(arr) 
-            if e['elementType'] == 'VARIABLESTUDIO':
-                VSID = e['id']
-     
-    if VSID != "":
+
+    return render_template('elements.html', elements=ELEMENTS)
+
+@app.route('/colors')
+def colors():
+    # Return a page that shows successful color shuffle
+    global USER_NAME
+    global PARTS
+    if request.args.get('pid'):
+        PID = request.args.get('pid')
+        newColor = {'red': randint(255), 'green': randint(255), 'blue': randint(255)}
+        # Get the current part properties
         response = requests.get(
             os.path.join(
-                OS_DOMAIN,
-                "api/variables/d/{}/w/{}/e/{}/variables".format(
-                    DID, WID, VSID
+                OS_DOMAIN, 
+                "api/metadata/d/{}/w/{}/e/{}/p/{}".format(
+                    DID, WID, EID, PID
                 )
             ), 
             headers={
@@ -81,47 +176,84 @@ def index():
                 "Authorization": "Bearer " + ACCESS_TOKEN
             }
         )
-        VARS = []
+        # Get the part properties for one particular part, given PID
+        partMetaData = response.json()
+        partProperties = partMetaData['properties']
+
+        # Replace the color property for that part
+        for i in range(len(partProperties)):
+            if partProperties[i]['name'] == 'Appearance':
+                partProperties[i]['value']['color'] = newColor
+            
+        partMetaData['properties'] = partProperties
+        payload = partMetaData
+
+        # Post the new properties
+        response = requests.post(
+            os.path.join(
+                OS_DOMAIN, 
+                "api/metadata/d/{}/w/{}/e/{}/p/{}".format(
+                    DID, WID, EID, PID
+                )
+            ), 
+            headers={
+                "Content-Type": "application/json", 
+                "Accept": "application/json;charset=UTF-8;qs=0.09", 
+                "Authorization": "Bearer " + ACCESS_TOKEN
+            },
+            json=payload
+        ) 
+
+        if response.ok:
+            for p in PARTS:
+                if p[1] == PID:
+                    r = f'{newColor["red"]:x}'
+                    g = f'{newColor["green"]:x}'
+                    b = f'{newColor["blue"]:x}'
+                    p[2] = r + g + b
+
+    else: 
+        # Get the current Onshape parts
+        response = requests.get(
+            os.path.join(
+                OS_DOMAIN, 
+                "api/parts/d/{}/w/{}/e/{}/".format(
+                    DID, WID, EID
+                )
+            ), 
+            headers={
+                "Content-Type": "application/json", 
+                "Accept": "application/json;charset=UTF-8;qs=0.09", 
+                "Authorization": "Bearer " + ACCESS_TOKEN
+            }
+        )
+        # Create array of parts [name, PID, hex RGB color]
         if response.ok: 
             response = response.json() 
-            variables = response[0]["variables"]
-            for v in variables:
-                arr = [v["name"], v["expression"]]
-                VARS.append(arr)
-
-    # Now let's try it without having to grab the Variable Studio EID.
-    response = requests.get(
-        os.path.join(
-            OS_DOMAIN,
-            "api/variables/d/{}/w/{}/e/{}/variables?includeValuesAndReferencedVariables=true".format(
-                DID, WID, EID
-            )
-        ), 
-        headers={
-            "Content-Type": "application/json", 
-            "Accept": "application/json;charset=UTF-8;qs=0.09", 
-            "Authorization": "Bearer " + ACCESS_TOKEN
-        }
+            PARTS = []
+            for i in range(len(response)):
+                r = f'{response[i]["appearance"]["color"]["red"]:x}'
+                g = f'{response[i]["appearance"]["color"]["green"]:x}'
+                b = f'{response[i]["appearance"]["color"]["blue"]:x}'
+                arr = [response[i]["name"], response[i]["partId"], r + g + b]
+                PARTS.append(arr)
+    return render_template(
+            "colors.html", parts=PARTS
     )
-    VARS2 = []
-    # if response.ok: 
-    #     response = response.json() 
-    #     print("response")
-    #     print(response)
-    #     variables = response[0]["variables"]
-    #     for v in variables:
-    #         arr = [v["name"], v["expression"]]
-    #         VARS2.append(arr)
-    #VARS = [1,2,3]
-    return render_template('index.html', parts=PARTS, elements=ELEMENTS, vsid=VSID, vars=VARS, vars2=VARS2, config=CONFIG, etype=ETYPE)
 
-# === Oauth stuff ===
+# The right panel app goes to this URL:
+# http://localhost:8000/login
+# And it sends these query string parameters:
+# did={$documentId}&wvm={$workspaceOrVersion}&wvmid={$workspaceOrVersionId}&eid={$elementId}&config={$configuration}
+
+# === Oauth stuff.  This happens first ===
 @app.route("/login/")
-def login(): 
+def login():
+    print("Logging in") 
     # When the app extension is first opened, this app page is called. 
     # It collects user information and redirects the user to Onshape's OAuth authorization page. 
     global ACCESS_TOKEN, REFRESH_TOKEN, EXPIRES_AT
-    global OS_DOMAIN, OS_USER_ID, DID, WID, EID, CONFIG, ETYPE
+    global OS_DOMAIN, OS_USER_ID, DID, WID, EID, CONFIG, FUNCTION, HOST
 
     # set the domain, user ID, DID, WID, EID, CONFIG for use later
     OS_DOMAIN = request.args.get('server')
@@ -129,13 +261,14 @@ def login():
     DID = request.args.get('did')
     WID = request.args.get('wvmid')
     EID = request.args.get('eid')
+    FUNCTION = request.args.get('function')
     extension_config = request.args.get('config')
     if extension_config == '{$configuration}':
         CONFIG = "Element not configured"
     else:
         CONFIG = extension_config
-    ETYPE = request.args.get('etype')
-
+    HOST = request.host.split(':')[1]
+    print(HOST)
     # If we don't have an access token, go get one.
     if not ACCESS_TOKEN: 
         return redirect(
@@ -159,10 +292,11 @@ def login():
         REFRESH_TOKEN = response['refresh_token']
         EXPIRES_AT = datetime.now() + timedelta(seconds=response['expires_in'])
 
-    return redirect(url_for("index"))   
+    return handleReturn(FUNCTION)
 
 @app.route("/oauthRedirect/")
 def authorize(): 
+    print("authorizing")
     # When the user authorizes the OAuth integration, Onshape's OAuth 
     # authorization page will redirect the user to this page, as registered as the redirect URL.
  
@@ -186,4 +320,4 @@ def authorize():
     REFRESH_TOKEN = response['refresh_token']
     EXPIRES_AT = datetime.now() + timedelta(seconds=response['expires_in'])
     
-    return redirect(url_for("index"))
+    return handleReturn(FUNCTION)
